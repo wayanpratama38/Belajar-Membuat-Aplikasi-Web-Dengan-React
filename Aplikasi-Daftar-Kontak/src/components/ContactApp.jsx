e964a1f03ea405605a3242789d7242c7b476fab4
wayanpratama38 <wayanpratama38@gmail.com>
2026-01-05 05:31:30 +0700

    feat : add form input and add new contact into contact list

--- Aplikasi-Daftar-Kontak/src/components/ContactApp.jsx
+++ Aplikasi-Daftar-Kontak/src/components/ContactApp.jsx
@@ -1,6 +1,7 @@
 import react from 'react';
 import ContactList from '../components/ContactList.jsx';
 import { getData } from '../data.js'
+import ContactInput from './ContactInput.jsx';
 export default class ContactApp extends react.Component {
   constructor(props) {
     super(props)
@@ -10,11 +11,29 @@ export default class ContactApp extends react.Component {
     }
 
     this.onDeleteHandler = this.onDeleteHandler.bind(this)
+    this.onAddContactHandler = this.onAddContactHandler.bind(this)
+
   }
 
   onDeleteHandler(id) {
     const contacts = this.state.contacts.filter(contact => contact.id !== id)
     this.setState({ contacts })
+  }
+
+  onAddContactHandler({ name, tag }) {
+    this.setState((prevState) => {
+      return {
+        contacts: [
+          ...prevState.contacts,
+          {
+            id: +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/default.jpg'
+          }
+        ]
+      }
+    })
   }
 
 
@@ -23,6 +42,8 @@ export default class ContactApp extends react.Component {
       <div>
         <h1>Daftar Kontak</h1>
         <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
+        <h1>Tambah Kontak</h1>
+        <ContactInput addContact={this.onAddContactHandler} />
       </div>
     )
   }
